47e5d599f1a5acf541c371f47f606f5c9ab0214a
xy <xiyain_01@163.com>
2020-01-06 18:54:50 +0800

    懒加载

--- spring-cloud/test/src/main/java/com/xy/test/single/LazyInnerClassSingleton.java
+++ spring-cloud/test/src/main/java/com/xy/test/single/LazyInnerClassSingleton.java
@@ -7,6 +7,9 @@ public class LazyInnerClassSingleton {
 
     private LazyInnerClassSingleton() {
         System.out.println("----LazyInnerClassSingleton----无参构造函数");
+        if (null == LazyHolder.LAZY_INNER_CLASS_SINGLETON) {
+            throw new RuntimeException("不允许创建多个实例");
+        }
     }
 
     public static LazyInnerClassSingleton getInstance() {
