488500d21f50f909b669231890741e87a7f61273
xiayin <xiayin_01@163.com>
2020-05-13 14:27:32 +0800

    依赖模块打包之类找不到

--- spring-cloud/auth-server/src/main/java/com/xy/auth/controller/LoginController.java
+++ spring-cloud/auth-server/src/main/java/com/xy/auth/controller/LoginController.java
@@ -1,10 +1,10 @@
 package com.xy.auth.controller;
 
+import com.xy.common.web.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,11 +26,10 @@ public class LoginController {
      * @return 结果
      */
     @DeleteMapping(value = "/exit")
-    public @ResponseBody
-    com.xy.common.web.ResponseBody revokeToken(String access_token) {
+    public ResponseBody<String> revokeToken(String access_token) {
         if (consumerTokenServices.revokeToken(access_token)) {
-            return com.xy.common.web.ResponseBody.success("注销成功");
+            return ResponseBody.success("注销成功");
         }
-        return com.xy.common.web.ResponseBody.fail("注销失败");
+        return ResponseBody.fail("注销失败");
     }
 }
